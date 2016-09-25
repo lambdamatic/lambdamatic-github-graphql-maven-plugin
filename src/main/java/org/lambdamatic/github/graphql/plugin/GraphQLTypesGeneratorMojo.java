3dfabf7ac1ee4c6fed3e77dbabf4dbf2959542c2
Xavier Coulon <xcoulon@redhat.com>
2016-09-25 08:22:56 +0200

    Added basic Mojo class
    Signed-off-by: Xavier Coulon <xcoulon@redhat.com>

---
+++ src/main/java/org/lambdamatic/github/graphql/plugin/GraphQLTypesGeneratorMojo.java
@@ -0,0 +1,30 @@
+package org.lambdamatic.github.graphql.plugin;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+@Mojo( name = "generate", defaultPhase = LifecyclePhase.PROCESS_SOURCES )
+public class GraphQLTypesGeneratorMojo extends AbstractMojo {
+    /**
+     * Location of the file.
+     */
+    @Parameter( defaultValue = "${project.build.directory}", property = "outputDir", required = true )
+    private File outputDirectory;
+
+    public void execute()
+        throws MojoExecutionException {
+        final File f = outputDirectory;
+        if (!f.exists()) {
+            f.mkdirs();
+        }
+    }
+}
